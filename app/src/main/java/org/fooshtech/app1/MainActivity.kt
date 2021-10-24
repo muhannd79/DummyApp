a9b80cc6909948a862ec09daa9584fd1a3219b64
Muhanned <muhannddh@gmail.com>
2021-10-24 18:05:57 -0500

    my final correct stage Video 3

--- app/src/main/java/org/fooshtech/app1/MainActivity.kt
+++ app/src/main/java/org/fooshtech/app1/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is contained in my Second Commit")
+        println("Local Changed")
     }
 }
