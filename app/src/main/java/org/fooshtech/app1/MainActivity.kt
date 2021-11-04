2d25c37f4fc760a70bc7c787296601716a5b5602
Muhanned <muhannddh@gmail.com>
2021-11-04 09:12:20 -0500

    has been changed in master branch commit

--- app/src/main/java/org/fooshtech/app1/MainActivity.kt
+++ app/src/main/java/org/fooshtech/app1/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is contained in my Second Commit")
-        println("Local Changed")
+        println("Local Changed has been changed in master")
         println("Change 1 in Master branch")
     }
 
